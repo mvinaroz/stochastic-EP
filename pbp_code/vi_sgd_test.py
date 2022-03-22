2d5862ec2d074b1c00f8fea0aee37ab178ad6197
mvinaroz <66011796+mvinaroz@users.noreply.github.com>
2022-03-21 18:08:22 -0700

    vi_sgd_test with KL div updated and minor changes

--- pbp_code/vi_sgd_test.py
+++ pbp_code/vi_sgd_test.py
@@ -49,8 +49,8 @@ class NN_Model(nn.Module):
         pred_samps =  self.relu(x_W_0_plus_bias) # num data samples by d_h by num MC samples (N  time d_h times L)
 
         ### KL term
-        trm1 = 0.5*(self.lamb*torch.sum(v) + self.lamb*torch.sum(m**2) - self.data_dim - torch.sum(torch.log(self.lamb*v)))
-        trm2 = 0.5*(self.lamb*torch.sum(v_pri) + self.lamb*torch.sum(m_pri**2) - d_h - torch.sum(torch.log(self.lamb*v_pri)))
+        trm1 = 0.5*(self.lamb*torch.sum(v) + self.lamb*torch.sum(m**2) - self.data_dim - self.data_dim*np.log(self.lamb) - torch.sum(torch.log(v)))
+        trm2 = 0.5*(self.lamb*torch.sum(v_pri) + self.lamb*torch.sum(m_pri**2) - d_h - d_h*np.log(self.lamb) - torch.sum(torch.log(v_pri)))
         KL_term = trm1 + trm2
 
         return pred_samps, KL_term, m_pri, v_pri
@@ -60,11 +60,15 @@ def loss_func(pred_samps, KL_term, y, gam, data_dim, m_pri, v_pri):
     # size(pred_samps) = num data samples by d_h by num MC samples
     n = pred_samps.shape[0]
 
+    print("This is data_dim:", data_dim)
+
     # m_pri times pred_samps
     m_pri_pred_samps = torch.einsum('j, njk -> nk', m_pri, pred_samps) # N by MC_samps
     y_m_pri_pred_samps = torch.einsum('n, nk -> nk', y, m_pri_pred_samps) # N by MC_samps
     trm1 = torch.sum(y**2)
+    print("This is trm1: ", trm1)
     trm2 = torch.mean(torch.sum(2*y_m_pri_pred_samps, 0))
+    print("This is trm2: ", trm2)
     trm3 = torch.mean(torch.sum(torch.einsum('njk,j -> nk', pred_samps**2, v_pri),0))
     trm4 = torch.mean(torch.sum(m_pri_pred_samps**2,0))
     trm5 = 0.5*n*data_dim*torch.log(2*torch.pi/gam)
@@ -72,13 +76,16 @@ def loss_func(pred_samps, KL_term, y, gam, data_dim, m_pri, v_pri):
     out = out1 + KL_term
     # print("This is out: ", out)
 
+    print("This is KL div term: ", KL_term )
+    print("This is the expectation term: ", out1)
+
     return out
 
 def generate_data(n, data_dim, n_hidden, lamb, gam):
 
     #Generate data features from N(0,I)
 
-    X_train=np.random.standard_normal(size=(n, data_dim))
+    X_train=np.random.randn(n, data_dim)
 
     #Weights true mean
     len_W0=(data_dim+1)*n_hidden
@@ -86,7 +93,7 @@ def generate_data(n, data_dim, n_hidden, lamb, gam):
     w_true_mean=np.random.normal(0, 1, len_W0 + len_w1) 
 
     #Generate true W0 and w1 (n by len_W0 + len w1)
-    w_i_true=np.random.normal(0, size=(n, len_W0 + len_w1))*np.sqrt(1./lamb) + np.tile(w_true_mean, (n, 1))
+    w_i_true=np.random.randn(n, len_W0 + len_w1)*np.sqrt(1./lamb) + np.tile(w_true_mean, (n, 1))
     w_i_0_plus_bias=np.reshape(w_i_true[:, 0:len_W0],(n, data_dim + 1, n_hidden))
     w_i_0_true=w_i_0_plus_bias[:, 0:data_dim, :]
     bias=w_i_0_plus_bias[:, -1, :]
@@ -103,7 +110,7 @@ def generate_data(n, data_dim, n_hidden, lamb, gam):
     mean_y=np.einsum('dn,nd -> n', w_i_1_true.T, relu_act) #w1.T * sig(W0*x + b)
 
     #Now we draw the labels from the model (N(y_i | f_{w_i}(x_i, gamma^{-1})))
-    y_labels=np.random.standard_normal(n)*np.sqrt(1./gam) +  mean_y
+    y_labels=np.random.randn(n)*np.sqrt(1./gam) +  mean_y
     #print(y_labels)
 
     return X_train, y_labels
@@ -118,28 +125,34 @@ def main():
     gam=10 #noise precision
     d_h=50
     num_samps=500 #MC samples
-    epochs=1000
+    epochs=10
+    normalize_data=False
+
     np.random.seed(0)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     X_train, y_train = generate_data(n, d, d_h, lamb, gam)
     X_test, y_test=generate_data(n_test, d, d_h, lamb, gam)
 
-    std_X_train = np.std(X_train, 0)
-    std_X_train[std_X_train == 0] = 1
-    mean_X_train = np.mean(X_train, 0)
+    if normalize_data:
+        print("We are normalizing the data")
+        std_X_train = np.std(X_train, 0)
+        std_X_train[std_X_train == 0] = 1
+        mean_X_train = np.mean(X_train, 0)
 
-    X_train = (X_train - np.full(X_train.shape, mean_X_train)) / \
-            np.full(X_train.shape, std_X_train)
+        X_train = (X_train - np.full(X_train.shape, mean_X_train)) / \
+                np.full(X_train.shape, std_X_train)
 
-    X_test = (X_test - np.full(X_test.shape, mean_X_train)) / \
-                 np.full(X_test.shape, std_X_train)
+        X_test = (X_test - np.full(X_test.shape, mean_X_train)) / \
+                np.full(X_test.shape, std_X_train)
 
-    mean_y_train = np.mean(y_train)
-    std_y_train = np.std(y_train)
-    print("This is std_y_train: ", std_y_train)
+        mean_y_train = np.mean(y_train)
+        std_y_train = np.std(y_train)
+        print("This is std_y_train: ", std_y_train)
 
-    y_train = (y_train - mean_y_train) / std_y_train
+        y_train = (y_train - mean_y_train) / std_y_train
+    else:
+        print("We aren't normalizing the data")
 
     len_m = d_h * (d + 1)  # length of mean parameters for W_0, where the size of W_0 is d_h by (d+1)
     #print("This is len_m: ", len_m)
@@ -152,7 +165,12 @@ def main():
     
     model = NN_Model(len_m, len_m_pri, len_v, num_samps, ms_vs, device, lamb, d)
     optimizer = optim.SGD(model.parameters(), lr=1e-3)
-    v_noise =  (1./gam) * std_y_train ** 2 #the variance for the noise and std_y_train ** 2 is because it's reescaling it.
+
+    if normalize_data:
+        v_noise =  (1./gam) * std_y_train ** 2 #the variance for the noise and std_y_train ** 2 is because it's reescaling it.
+    else:
+        v_noise =  (1./gam)
+
     print("This is v_noise: ", v_noise)
 
     for epoch in range(1, epochs + 1):
@@ -177,8 +195,10 @@ def main():
         w_1_times_pred_samps_y_tst = torch.einsum('jk,  njk -> nk', samples_w_1, pred_samps_y_tst)
         m_prd = (torch.mean(w_1_times_pred_samps_y_tst, 1)).detach().numpy()
         v_prd = (torch.var(w_1_times_pred_samps_y_tst, 1)).detach().numpy()
-        m_prd = m_prd * std_y_train + mean_y_train #Reescale due to normalization of data.
-        v_prd = v_prd * std_y_train ** 2 #Reescale due to normalization of data.
+
+        if normalize_data:
+            m_prd = m_prd * std_y_train + mean_y_train #Reescale due to normalization of data.
+            v_prd = v_prd * std_y_train ** 2 #Reescale due to normalization of data.
 
         test_ll = np.mean(-0.5 * np.log(2 * math.pi * (v_prd + v_noise)) - \
                           0.5 * (y_test - m_prd) ** 2 / (v_prd + v_noise))
@@ -196,9 +216,3 @@ if __name__ == '__main__':
 
 
     
-
-
-
-
-
-
