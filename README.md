6b4972812a552c4262c37db7bdeb3f7f5aba6cd1
mvinaroz <margarita.vinaroz@gmail.com>
2022-06-07 13:20:21 -0700

    final code version

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Versions numbers are based on our system and may not need to be exact matches.
     matplotlib 3.4.3
     numpy 1.21.2
     pandas 1.3.3
+    pytorch 1.10.1
 
 ## Repository Structure
 
@@ -22,7 +23,7 @@ Versions numbers are based on our system and may not need to be exact matches.
 Contains the code for computing the DP-SEP experiments for MoG clustering.
 
 - For plotting Figure 1 run the following command: `python plot_cluster.py --is-private --num-data 1000 --num-iter 100 --c 1 --gamma 1 --delta 1e-5`
-- For obtaining Table 1 errors run:
+- For obtaining Table 1 F-norms run:
     - SEP: `python demo_dpsep.py --num-data 1000 --dimension 4 --num_group 4 --num-iter 100 --gamma 1`
     - clipped SEP: `python demo_dpsep.py --num-data 1000 --dimension 4 --num_group 4 --num-iter 100 --gamma 1 --clip --c 1` for different c values.
     - DP-SEP: `python demo_dpsep.py --num-data 1000 --dimension 4 --num_group 4 --num-iter 100 --gamma 1 --clip --is-private --epsilon 1 --delta 1e-5` 
@@ -33,9 +34,11 @@ Contains the code for computing the DP-SEP experiments for MoG clustering.
 ### pbp_code
 
 Contains pbp experiments for regression datasets using EP, SEP, clipped SEP and DP-SEP. 
-- To obtain Table 2 and Table 3 different seed errors with:
-    - EP: `python pbp_regression_ep.py --seed 1 --data-name naval --n-hidden 50 --epochs 40`
-    - SEP: `python pbp_regression_sep.py --seed 1 --data-name naval --n-hidden 50 --epochs 40`
-    - clipping SEP: `python pbp_regression_dpsep.py --seed 1 --data-name naval --n-hidden 50 --epochs 40 --clip --c 1`
-    - DP-SEP: `python pbp_regression_dpsep.py --seed 1 --data-name naval --n-hidden 50 --epochs 40 --clip --c 1 --is-private --epsilon 1 --delta 1e-5`
+- To obtain Table 3 and Table 4 different seed errors with:
+    - EP: `python pbp_regression_ep.py --seed 1 --data-name wine --n-hidden 50 --epochs 40`
+    - SEP: `python pbp_regression_sep.py --seed 1 --data-name wine --n-hidden 50 --epochs 40`
+    - clipping SEP: `python pbp_regression_dpsep.py --seed 1 --data-name wine --n-hidden 50 --epochs 40 --clip --c 1`
+    - DP-SEP: `python pbp_regression_dpsep.py --seed 1 --data-name wine --n-hidden 50 --epochs 40 --clip --c 1 --is-private --epsilon 1 --delta 1e-5`
+    -VI:  `python vi_sgd_real_data.py --seed 1 --data-name wine  --n-hidden 50 --epochs 40 --lamb 200 --gam 2 --mc-samps 20 --beta 0.01`
+    - DP-VI:  `python vi_sgd_real_data.py --seed 1 --data-name wine  --n-hidden 50 --epochs 40 --lamb 200 --gam 2 --mc-samps 20 --beta 0.01 --clip 5 --delta 1e-5 --sigma 30`
 
--- pbp_code/vi_test_real_data_dp.py
+++ pbp_code/vi_test_real_data_dp.py
@@ -13,9 +13,6 @@ from aux_files import load_data
 
 from autodp import rdp_acct, rdp_bank
 
-import matplotlib
-from matplotlib import pyplot
-#matplotlib.pyplot.switch_backend('agg')
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
@@ -24,7 +21,6 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 class NN_Model(nn.Module):
 
     def __init__(self,  len_m, len_v, num_samps_w0, num_samps_w1, init_var_params, device, lamb, d_h):
-        # len_m, len_m_pri, len_v, num_samps, ms_vs, device, gam, lamb, d
         super(NN_Model, self).__init__()
         self.parameter = Parameter(torch.Tensor(init_var_params), requires_grad=True)
         self.num_MC_samps_w0 = num_samps_w0
@@ -38,9 +34,6 @@ class NN_Model(nn.Module):
         self.relu = F.relu
         self.d_h = d_h
 
-        # self.random_ness = random_ness
-   
-
     def forward(self, x):  # x is mini_batch_size by input_dim
 
 
@@ -50,12 +43,10 @@ class NN_Model(nn.Module):
 
 
         v_w0 = torch.abs(ms_vs[self.len_m + self.m_pri: self.len_m + self.m_pri + self.len_m])
-        #v_w0=self.relu(ms_vs[self.len_m + self.m_pri: self.len_m + self.m_pri + self.len_m])
         v_w0=v_w0 + 1e-6*torch.ones(v_w0.size())
    
 
         v_pri=torch.abs(ms_vs[self.len_m + self.m_pri + self.len_m:])
-        #v_pri=self.relu(ms_vs[self.len_m + self.m_pri + self.len_m:])
         v_pri=v_pri + 1e-6*torch.ones(v_pri.size())
 
 
@@ -70,15 +61,6 @@ class NN_Model(nn.Module):
         data_dim = int(self.len_m / self.d_h - 1)
         W0=torch.reshape(samps_w0, (data_dim + 1, self.d_h, self.num_MC_samps_w0))
 
-        #x_w0=torch.zeros((x.shape[0], self.d_h, self.num_MC_samps_w0))
-
-
-        #for i in range(0, self.d_h):
-        #    for j in range(0, self.num_MC_samps_w0):
-                # x has shape N by d+1
-                # W0[:, i, j] has shape d+1 
-        #        x_w0[:, i, j]=torch.matmul(x, W0[:, i, j]) 
-
         x_w0=torch.einsum('nd, dhm -> nhm', x, W0)
            
         z=self.relu(x_w0)
@@ -90,52 +72,11 @@ class NN_Model(nn.Module):
         for i in range(0, self.num_MC_samps_w1):
             samps_w1[:, i]= m_pri + torch.randn(m_pri.shape)*torch.sqrt(v_pri)
             
-        #pred_samps=torch.zeros((x.shape[0], self.num_MC_samps_w0, self.num_MC_samps_w1))
-
-        #for i in range(0, self.num_MC_samps_w0):
-        #    pred_samps[:, i, :]=torch.matmul(z[:,:, i], samps_w1)
 
         pred_samps=torch.einsum('ndm, dc -> nmc', z, samps_w1)
 
-        #print("Check if pred_samps and pred_samps_einsum are equal: ", torch.div( pred_samps, pred_samps_einsum) - 1)
 
         return pred_samps, m_w0,  v_w0, m_pri, v_pri
-
-
-def kl_div_term(m0, v0, lamb):
-
-    # KL(N0, N1) = 0.5*(TR(\precision1 \cov0) + (\mu1 - \mu0).T \precision1 (\mu1 - \mu0) -d + ln( det(\cov1) / det(\cov0) ) )
-    #print("This is v0: ", v0)
-    cov0=torch.diag(v0)
-    #print("This is cov0: ", cov0)
-    cov1=torch.eye(v0.shape[0])*(1/torch.tensor(lamb))
-    
-    precision1=torch.linalg.inv(cov1)
-
-    m1=torch.zeros(m0.shape)
-
-    trace_trm=torch.trace(torch.matmul(precision1, cov0))
-    #print("This is trace_term: ", trace_trm)
-
-    mu_diff=m1 - m0
-    #print("This is m1 - m0: ", mu_diff)
-    squared_term=torch.matmul(torch.matmul(mu_diff.T, precision1), mu_diff)
-    #print("This is squared_term: ", squared_term)
-
-    d=m0.shape[0]
-    #print("This is dim in kl:", d)
-
-    
-    log_term=torch.log(torch.det(cov1))  - torch.log(torch.det(cov0))
-    #print("This is torch.det(cov1): ", torch.det(cov1))
-    #print("This is torch.log(torch.det(cov1)): ", torch.log(torch.det(cov1)))
-    #print("This is torch.log(torch.det(cov0)): ", torch.log(torch.det(cov0)))
-    #print("This is torch.det(cov1) / torch.det(cov0): ", torch.det(cov1) / torch.det(cov0))
-    #print("This is log_term: ", log_term)
-
-    kl_div=0.5*(trace_trm + squared_term - d + log_term )
-
-    return kl_div
 
 
 def kl_div_univariate(m_0, v_0, lamb):
@@ -180,8 +121,6 @@ def privacy_param_func(sigma, delta, n_epochs, batch_size, n_data):
     k = n_epochs  # k is the number of steps during the entire training
     prob = batch_size / n_data  # prob is the subsampling probability
 
-    """ end of input arguments """
-
     """ now use autodp to calculate the cumulative privacy loss """
     # declare the moment accountants
     acct = rdp_acct.anaRDPacct()
@@ -218,16 +157,13 @@ def get_args():
     parser.add_argument('--lr', type=float, default=1e-2,  help='learning rate' )
     parser.add_argument('--lamb', type=float, default=200,  help='precision on weights' )
     parser.add_argument('--gam', type=float, default=2,  help='precision on noise' )
-    parser.add_argument('--mc-samps-w0', type=int, default=10,  help='number of mc samples to generate for w0' )
-    parser.add_argument('--mc-samps-w1', type=int, default=10,  help='number of mc samples to generate for w1' )
+    parser.add_argument('--mc-samps', type=int, default=20,  help='number of mc samples to generate' )
     parser.add_argument('--beta', type=float, default=0.01,  help='' )
 
-    parser.add_argument('--clip', type=float, default=10,  help='' )
+    #DP ARGUMENTS
+    parser.add_argument('--clip', type=float, default=5,  help='' )
     parser.add_argument('--delta', type=float, default=1e-5,  help='' )
     parser.add_argument('--sigma', type=float, default=30,  help='' )
-
-
-
 
 
     ar = parser.parse_args()
@@ -245,8 +181,8 @@ def main():
     print(device)
 
     d_h=ar.n_hidden
-    n_MC_samps_w0=ar.mc_samps_w0
-    n_MC_samps_w1=ar.mc_samps_w1
+    n_MC_samps_w0=ar.mc_samps
+    n_MC_samps_w1=ar.mc_samps
     lamb=ar.lamb
     gam=ar.gam
     epochs=ar.epochs
@@ -258,7 +194,6 @@ def main():
 
 
     """Load data"""
-    # Load data.
     X_train, X_test, y_train, y_test = load_data(ar.data_name, ar.seed)
 
      # Normalizing data
@@ -284,7 +219,7 @@ def main():
     else:
         print('testing non-standardized data')
 
-    v_noise =  (1./ar.gam) * std_y_train ** 2 #the variance for the noise and std_y_train ** 2 is because it's reescaling it.
+    v_noise =  (1./ar.gam) * std_y_train ** 2 
 
     print("This is v_noise: ", v_noise)
 
@@ -314,11 +249,8 @@ def main():
     len_v_pri = d_h + 1 # length of variance parameters for w_1
     init_ms = 1*torch.randn(len_m + len_m_pri) # initial values for all means
     init_vs = 1*torch.randn(len_v + len_v_pri) # initial values for all variances
-    #init_vs = 1*torch.ones(len_v + len_v_pri)
     ms_vs = torch.cat((init_ms, init_vs), 0)
 
-
-    #print("This is ms_vs: ", ms_vs.shape)
 
     model = NN_Model(len_m, len_v, n_MC_samps_w0, n_MC_samps_w1, ms_vs, device, lamb, d_h)
     optimizer = optim.SGD(model.parameters(), lr=ar.lr)
@@ -344,7 +276,6 @@ def main():
             
             kl_w1=torch.sum(kl_w1_dh)
             #print("This is kl_w1: ", kl_w1)
-            #kl_w1=0
 
             kl_w0_dh=torch.zeros(m_w0.shape[0])
 
@@ -352,16 +283,13 @@ def main():
                 kl_w0_dh[dim]=kl_div_univariate(m_w0[dim], v_w0[dim], lamb)
 
             kl_w0=torch.sum(kl_w0_dh)
-            #kl_w0=0
             #print("This is kl_w0: ", kl_w0)
             kl_term=(ar.beta/batch_size)*(kl_w0 + kl_w1)
-            #kl_term=0
 
             loss_list=[]
             accumulated_grads=[]
 
             for item in range(0, batch_size):
-                #print("This is pred_samps[item]: ", labels[item])
                 loss_item=loss_func( torch.unsqueeze(pred_samps[item], 0), labels[item], torch.tensor(gam), kl_term)
                 loss_list.append(loss_item)
                 loss_item.backward(retain_graph=True)
@@ -387,7 +315,7 @@ def main():
 
             
 
-            priv_grads=  (aggregated_grad + gaussian_noise)/batch_size
+            priv_grads= (aggregated_grad + gaussian_noise)/batch_size
             
             for p in model.parameters():
                 p.grad=priv_grads
@@ -401,24 +329,7 @@ def main():
 
         """Compute y predicted from model parameters to compare it with y_test"""
 
-        #print("This are the mean and std for y: ,", torch.mean(y), torch.std(y))
-        #print("This are the mean and std for y_pred: ,", torch.mean(y_pred), torch.std(y_pred))
-
         pred_samps_test, m_w0_test, v_w0_test, m_w1_test, v_w1_test = model(X_test)
-
-        W0_test=torch.randn((d+1)*d_h)*torch.sqrt(v_w0_test) +  m_w0_test
-        W0_test=torch.reshape(W0_test, (d + 1, d_h))
-
-        x_w0_test=torch.mm(X_test,  W0_test)
-
-        z0_test=F.relu(x_w0_test)
-        z0_test=torch.cat((torch.ones(n_test,1), z0_test), 1)
-
-        w1_test=torch.randn(d_h + 1)*torch.sqrt(v_w1_test) + m_w1_test
-
-        z1_test=torch.matmul(z0_test,  w1_test) 
-
-        #print("This is pred_samps_test: ", pred_samps_test.shape)
 
         m_prd = (torch.mean(pred_samps_test, (1, 2))).detach().numpy()
         v_prd = (torch.var(pred_samps_test, (1, 2))).detach().numpy()
@@ -428,9 +339,6 @@ def main():
             v_prd = v_prd * std_y_train ** 2
 
         y_test_numpy=y_test.detach().numpy()
-    
-
-        y_test_pred=torch.randn(n_test)*torch.sqrt(1/torch.tensor(gam)) + z1_test
 
         test_ll = np.mean(-0.5 * np.log(2 * math.pi * (v_prd + v_noise)) - \
                           0.5 * ( y_test_numpy - m_prd) ** 2 / (v_prd + v_noise))
@@ -440,27 +348,5 @@ def main():
         print("rmse: ", rmse)
 
  
-
-        
-    #matplotlib.pyplot.figure(figsize = [10, 5]) # larger figure size for subplots
-
-    # example of somewhat too-large bin size
-    #matplotlib.pyplot.subplot(1, 2, 1) # 1 row, 2 cols, subplot 1
-
-    #matplotlib.pyplot.hist(y_test.squeeze().detach().numpy(), bins=20)
-    #matplotlib.pyplot.xlabel('y_test')
-
-    # example of somewhat too-small bin size
-    #matplotlib.pyplot.subplot(1, 2, 2) # 1 row, 2 cols, subplot 2
-
-
-    #matplotlib.pyplot.hist(y_test_pred.squeeze().detach().numpy(),  bins=20)
-    #matplotlib.pyplot.xlabel('y_test_pred')
-    #matplotlib.pyplot.show()
-
-
-
-
-
 if __name__ == '__main__':
     main()
